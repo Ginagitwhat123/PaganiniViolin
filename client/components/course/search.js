703eb23f369c116a5f350c558acd9031572745e5
Gina <ginafhd4219@gmail.com>
2025-08-26 10:53:30 +0800

    git init

---
+++ client/components/course/search.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from './search.module.scss'
+import { IoSearchSharp, IoCloseCircleOutline } from 'react-icons/io5'
+
+const SearchBar = ({ search, onSearchChange, onSearchClick, onClearSearch }) => {
+  return (
+    <div className={`mb-3 d-flex ${styles['search']}`}>
+      <input
+        type="text"
+        className={`form-control ${styles['search-bar']}`}
+        placeholder="搜尋"
+
+        value={search}
+        onChange={onSearchChange}
+      />
+      {search && (
+        <span
+          className={`btn ${styles['btn-clear']}`}
+          onClick={onClearSearch}
+        >
+          <IoCloseCircleOutline size={20} />
+        </span>
+      )}
+      <span
+        className={`btn input-group-text ${styles['btn-search']}`}
+        id="basic-addon2"
+        onClick={onSearchClick}
+      >
+        <IoSearchSharp size={20} />
+      </span>
+    </div>
+  );
+};
+
+export default SearchBar;
